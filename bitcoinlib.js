5d177eeafa752e4aca7067887728fbd92ba6ed07
bityogi <bityogi@tutanota.com>
2016-01-30 19:35:16 -0500

    generate a bitcoin address from hash using jslib

---
+++ bitcoinlib.js
@@ -0,0 +1,31 @@
+var bitcoin = require('bitcoinjs-lib');
+var bigi = require('bigi');
+
+//generating a bitcoin address from a hash
+function generateAddress(stringToHash) {
+  var hash = bitcoin.crypto.sha256(stringToHash);
+  var d = bigi.fromBuffer(hash);
+
+  var options = {
+    compressed: false,
+    network: bitcoin.networks.testnet
+  }
+
+  var keyPair = new bitcoin.ECPair(d, null, options);
+  var address = keyPair.getAddress();
+  var wif = keyPair.toWIF();
+
+  console.log('addressInfo:', address);
+  console.log('wif:', wif);
+
+}
+
+var stringtoHash = process.argv[2];
+
+if (!stringtoHash) {
+  console.log('No string to hash provided.');
+  process.exit(0);
+}
+console.log('stringHashed:', stringtoHash);
+
+generateAddress(stringtoHash);
